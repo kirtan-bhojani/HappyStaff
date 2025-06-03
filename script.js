82d048b8a529fb21ae97356c882767a5a658ffff
kirtan-bhojani <kirtan.bhojani@gmail.com>
2025-06-03 15:22:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  function toggleTheme() {
-  const html = document.documentElement;
-  const isDark = html.classList.toggle('dark');
-  localStorage.setItem('theme', isDark ? 'dark' : 'light');
-}
 
   // Initialize Feather Icons
   feather.replace();
